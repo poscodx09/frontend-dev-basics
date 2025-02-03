f75f8e8ac385379d81b69b1371cc79642f15e817
ymj07168 <ymj07168@gmail.com>
2025-02-03 16:51:11 +0900

    ex02

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -1,3 +1,10 @@
 console.log("============[02.02] 표현식(Expression)");
+parseInt('4');
+Math.pow(2, 2);
+
 console.log("============[05] 세미콜론(Semicolon)");
+a = 10; b = 20;
+
 console.log("============[06] 개행(Line Break)");
+i = 10;
+console.log(i);
