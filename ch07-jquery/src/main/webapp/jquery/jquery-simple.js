20041dbaa118d1670343acfd8c57b6dcb0838798
ymj07168 <ymj07168@gmail.com>
2025-02-05 16:49:08 +0900

    jquery

---
+++ ch07-jquery/src/main/webapp/jquery/jquery-simple.js
@@ -0,0 +1,33 @@
+var _jQuery = function(elements){
+	this.length = elements.length;
+	for (var i = 0; i < this.length; i++){
+		this[i] = elements[i];
+	}
+}
+
+_jQuery.prototype.click = function(callback){
+	for (var i = 0; i < this.length; i++){
+		this[i].addEventListener('click', callback);
+	}
+}
+
+_jQuery.prototype.css = function(name, value){
+	for (var i = 0; i < this.length; i++){
+		this[i].style[name] = value;
+	}
+}
+
+var jQuery = function(param){
+	if (typeof(param) === 'function'){
+		window.addEventListener('load', param);
+		return;
+	}
+	if (typeof(param) === 'string'){
+		elements = document.querySelectorAll(param);
+		
+	}
+	
+	return new _jQuery(elements);
+}
+
+var $ = jQuery;
