ec18ac618281ca57a7d7fb0868b967a6ca82fa78
ymj07168 <ymj07168@gmail.com>
2025-02-06 16:47:31 +0900

    class & array method

--- javascript-practices/es6 features/ex06.js
+++ javascript-practices/es6 features/ex06.js
@@ -31,4 +31,30 @@ const circle = new Circle(10, 20, 5);
 circle.draw();
 
 console.log('---[클래스 상속(extends) 지원]');
+class Shape {
+    constructor(fillColor, lineColor){
+        this.fillColor = fillColor;
+        this.lineColor = lineColor;
+    }
 
+    // @Override
+    draw(){
+        console.log("구현할 수 없습니다.")
+    }
+}
+
+class RectTriangle extends Shape {
+    constructor(w, h, fillColor, lineColor){
+        super(fillColor, lineColor);
+        this.w = w;
+        this.h = h;
+    }
+
+    // @Override
+    draw(){
+        console.log(`RectTriangle(w=${this.w}, h=${this.h}, fillColor=${this.fillColor}, lineColor=${this.lineColor})`)
+    }
+}
+
+const shape1 = new RectTriangle(10, 20, 'red', 'black');
+shape1.draw();
--- javascript-practices/es6 features/ex07.js
+++ javascript-practices/es6 features/ex07.js
@@ -13,13 +13,37 @@
 const  a = [1, 2, 3, 4, 5]
 
 
+// Array 함수 - forEach
+a.forEach((e, i) => console.log(`i:${i}, e:${e}`));
+
+// Array 함수 - map
+const a2 = a.map((e) => e*e);
+console.log(a2)
+
 // Array 함수 - filter
+const a3 = a.filter((e) => e%2 !== 0);
+console.log(a3);
 
 // Array 함수 - every
+const result = a.every((e) => e < 10);
+console.log(result);
 
 // Array 함수 - some
+const result2 = a.some(e => e % 5 === 0);
+console.log(result2);
+
+// Array 함수 - find
+const number = a.findIndex(e => e === 4);
+console.log(number);
 
 // Array 함수 - reduce
+let sum = 0;
+a.forEach(e => sum += e);
+console.log(sum);
+
+sum = a.reduce((acc, e) => {acc += e; return acc;}, 0)
+console.log(sum);
 
 // Array 함수 - reduceRight
-
+const a5 = a.reduceRight((acc, e)=> acc.concat(e*e), []);
+console.log(a5);
